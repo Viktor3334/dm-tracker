ba19282fd266844e3de7119d21ec5391e588dfcf
Viktor3334 <viktor.probst06@gmail.com>
2024-11-12 10:47:16 +0000

    Testing w/o JS

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const table = document.getElementById("initiative-list").getElementsByTagName("tbody")[0];
-  
-    // Add a sample row for demonstration
-    const newRow = table.insertRow();
-    newRow.innerHTML = '<td contenteditable="true"></td><td contenteditable="true"></td>';
-  });
   
